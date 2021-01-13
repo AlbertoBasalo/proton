e00c369305861d33e3a31c58581405a5ccd97a90
Alberto Basalo <albertobasalo@hotmail.com>
2021-01-13 11:10:11 +0100

    wip: categories repository class

--- src/api/categories.repository.ts
+++ src/api/categories.repository.ts
@@ -1,45 +1,50 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-const collection = [
-  { id: '1', name: 'Libraries', description: 'Packages or source code' },
-  { id: '2', name: 'UI components', description: 'Presentational frameworks and utilities' },
-];
 
-export function select(): unknown[] {
-  return collection;
-}
+class CategoriesRepository {
+  private readonly collection = [
+    { id: '1', name: 'Libraries', description: 'Packages or source code' },
+    { id: '2', name: 'UI components', description: 'Presentational frameworks and utilities' },
+  ];
 
-export function selectById(id: string): unknown {
-  return collection.find(x => x.id === id);
-}
+  public select(): unknown[] {
+    return this.collection;
+  }
 
-export function insert(toAdd: any): unknown {
-  const id = toAdd.id;
-  const index = collection.findIndex(x => x.id === id);
-  if (index >= 0) {
-    return null;
-  } else {
-    collection.push(toAdd);
-    return toAdd;
+  public selectById(id: string): unknown {
+    return this.collection.find(x => x.id === id);
+  }
+
+  public insert(toAdd: any): unknown {
+    const id = toAdd.id;
+    const index = this.collection.findIndex(x => x.id === id);
+    if (index >= 0) {
+      return null;
+    } else {
+      this.collection.push(toAdd);
+      return toAdd;
+    }
+  }
+
+  public update(id: string, toUpdate: any): unknown {
+    const index = this.collection.findIndex(x => x.id === id);
+    if (index >= 0) {
+      this.collection[index] = toUpdate;
+      return toUpdate;
+    } else {
+      return null;
+    }
+  }
+
+  public delete(id: string): unknown {
+    const index = this.collection.findIndex(x => x.id === id);
+    if (index >= 0) {
+      this.collection.splice(index, 1);
+      return true;
+    } else {
+      return null;
+    }
   }
 }
 
-export function update(id: string, toUpdate: any): unknown {
-  const index = collection.findIndex(x => x.id === id);
-  if (index >= 0) {
-    collection[index] = toUpdate;
-    return toUpdate;
-  } else {
-    return null;
-  }
-}
-
-export function remove(id: string): unknown {
-  const index = collection.findIndex(x => x.id === id);
-  if (index >= 0) {
-    collection.splice(index, 1);
-    return true;
-  } else {
-    return null;
-  }
-}
+export const categoriesRepository = new CategoriesRepository();

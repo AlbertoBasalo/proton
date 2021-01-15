c818fd51d09f2b25bf5bbe1ccbeb0982202c371c
Alberto Basalo <albertobasalo@hotmail.com>
2021-01-15 13:28:20 +0100

    wip: authorization

--- src/util/app/routes.ts
+++ src/util/app/routes.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import * as express from 'express';
 import * as OpenApiValidator from 'express-openapi-validator';
 import { Express } from 'express-serve-static-core';
+import * as jwt from 'jsonwebtoken';
 import { connector, summarise } from 'swagger-routes-express';
 import YAML from 'yamljs';
 import * as api from '../../api';
+import { httpStatus } from '../../models/httpStatus';
 import { routerConfig } from '../config';
 import { logger } from '../logger';
 
@@ -23,7 +26,11 @@ function validateApiDefinition(app: Express) {
 }
 
 function connectApiToControllers(apiDefinition: any, app: Express) {
-  const connect = connector(api, apiDefinition);
+  const connect = connector(api, apiDefinition, {
+    security: {
+      bearerAuth: auth,
+    },
+  });
   connect(app);
 }
 
@@ -32,4 +39,22 @@ function getApiDefinition() {
   const apiSummary = summarise(apiDefinition);
   logger.dump(apiSummary);
   return apiDefinition;
+}
+
+export function auth(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): void {
+  try {
+    const token = req.headers.authorization!;
+    logger.info(token);
+    const userToken = jwt.verify(token, 'Prot0n');
+    logger.dump(userToken);
+    req['userToken'] = userToken;
+  } catch (err) {
+    logger.warn(err);
+    req['userToken'] = null;
+    res.status(httpStatus.UNAUTHORIZED).json({ message: err.message });
+  }
 }

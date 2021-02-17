60851b65b87924b61ec6efa64418e492a78e6fbf
Alberto Basalo <albertobasalo@hotmail.com>
2021-02-17 17:14:10 +0100

    feat: Enable-CORS - Closes #30

--- src/util/app/app.ts
+++ src/util/app/app.ts
@@ -1,3 +1,4 @@
+import * as cors from 'cors';
 import express from 'express';
 import { Express } from 'express-serve-static-core';
 import { RootConfig } from '../../models/RootConfig';
@@ -7,6 +8,7 @@ import { connectOpenAPIRoutes } from './routes';
 
 export async function createApp(rootConfig: RootConfig): Promise<Express> {
   const app = express();
+  app.use(cors.default());
   processRequest(app, rootConfig);
   connectOpenAPIRoutes(app);
   responseErrorHandler(app, rootConfig);
